9c1b0c1d2c4161e028721bb802a0deef49b055e1
NgoThanhTan1811 <165416753+NgoThanhTan1811@users.noreply.github.com>
2025-06-18 20:15:58 +0700

    fix

--- BackEnd/BackEnd/settings.py
+++ BackEnd/BackEnd/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import pymysql
+pymysql.install_as_MySQLdb()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,10 +81,18 @@ WSGI_APPLICATION = 'BackEnd.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'face_attendances',      # tên database bạn vừa tạo
+        'USER': 'tan',                 # user bạn tạo
+        'PASSWORD': '',
+        'HOST': 'localhost',               # hoặc 'localhost'
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
+
 
 
 # Password validation
